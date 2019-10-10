1f5f937ba9acaf3fb53438edca16a9dac6cded05
aleksandr.antipin.1999@mail.ru <antipin.aleksandr.1999@mail.ru>
2019-10-10 17:43:31 +0300

    some fixes 2

--- modules/task_1/Antipin_A_scalar_product/main.cpp
+++ modules/task_1/Antipin_A_scalar_product/main.cpp
@@ -8,15 +8,18 @@
 TEST(scalar_product, can_product_vectors_with_100_range) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> vec1;
-    std::vector<int> vec2;
-    vec1 = creatRandomVector(100);
-    vec2 = creatRandomVector(100);
+    std::vector<int> vec1(100);
+    std::vector<int> vec2(100);
+    if (rank == 0) {
+        vec1 = creatRandomVector(100);
+        vec2 = creatRandomVector(100);
+    }
 
     int res1 = getParallelScalarProduct(vec1, vec2);
-    int res2 = getSequentialScalarProduct(vec1, vec2);
+    
     if (rank == 0) {
-         ASSERT_EQ(res1, res2);
+        int res2 = getSequentialScalarProduct(vec1, vec2);
+        ASSERT_EQ(res1, res2);
     }
 }
 
@@ -33,14 +36,17 @@ TEST(scalar_product, can_throw_if_product_vectors_with_different_ranges) {
 TEST(scalar_product, can_product_vectors_with_odd_range) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> vec1;
-    std::vector<int> vec2;
-    vec1 = creatRandomVector(125);
-    vec2 = creatRandomVector(125);
+    std::vector<int> vec1(125);
+    std::vector<int> vec2(125);
+    if (rank == 0) {
+        vec1 = creatRandomVector(125);
+        vec2 = creatRandomVector(125);
+    }
 
     int res1 = getParallelScalarProduct(vec1, vec2);
-    int res2 = getSequentialScalarProduct(vec1, vec2);
+    
     if (rank == 0) {
+        int res2 = getSequentialScalarProduct(vec1, vec2);
         ASSERT_EQ(res1, res2);
     }
 }
@@ -48,14 +54,17 @@ TEST(scalar_product, can_product_vectors_with_odd_range) {
 TEST(scalar_product, can_product_vectors_with_big_range) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> vec1;
-    std::vector<int> vec2;
-    vec1 = creatRandomVector(100000);
-    vec2 = creatRandomVector(100000);
+    std::vector<int> vec1(100000);
+    std::vector<int> vec2(100000);
+    if (rank == 0) {
+        vec1 = creatRandomVector(100000);
+        vec2 = creatRandomVector(100000);
+    }
 
     int res1 = getParallelScalarProduct(vec1, vec2);
-    int res2 = getSequentialScalarProduct(vec1, vec2);
+    
     if (rank == 0) {
+        int res2 = getSequentialScalarProduct(vec1, vec2);
         ASSERT_EQ(res1, res2);
     }
 }
@@ -63,14 +72,17 @@ TEST(scalar_product, can_product_vectors_with_big_range) {
 TEST(scalar_product, can_product_vectors_with_very_big_range) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    std::vector<int> vec1;
-    std::vector<int> vec2;
-    vec1 = creatRandomVector(1000000);
-    vec2 = creatRandomVector(1000000);
+    std::vector<int> vec1(1000000);
+    std::vector<int> vec2(1000000);
+    if (rank == 0) {
+        vec1 = creatRandomVector(1000000);
+        vec2 = creatRandomVector(1000000);
+    }
 
     int res1 = getParallelScalarProduct(vec1, vec2);
-    int res2 = getSequentialScalarProduct(vec1, vec2);
+    
     if (rank == 0) {
+        int res2 = getSequentialScalarProduct(vec1, vec2);
         ASSERT_EQ(res1, res2);
     }
 }
--- modules/task_1/Antipin_A_scalar_product/scalar_product.cpp
+++ modules/task_1/Antipin_A_scalar_product/scalar_product.cpp
@@ -34,28 +34,48 @@ int getParallelScalarProduct(const std::vector<int>& a, const std::vector<int>& 
     }
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     int v_size = a.size() / size;
+    int re = a.size() % size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int result = 0, result_other = 0, resive = 0;
+    int result = 0, result_other = 0;
+
     if (rank == 0) {
-        for (int i = 0; i < v_size; i++) {
-            result += a[i] * b[i];
+        for (int proc = 1; proc < size; ++proc) {
+            if (proc == size - 1) {
+                MPI_Send(&a[0] + proc * v_size, v_size + re, MPI_INT, proc, 2, MPI_COMM_WORLD);
+                MPI_Send(&b[0] + proc * v_size, v_size + re, MPI_INT, proc, 3, MPI_COMM_WORLD);
+            } else {
+                MPI_Send(&a[0] + proc * v_size, v_size, MPI_INT, proc, 0, MPI_COMM_WORLD);
+                MPI_Send(&b[0] + proc * v_size, v_size, MPI_INT, proc, 1, MPI_COMM_WORLD);
+            }
         }
-        for (int i = 1; i < size; i++) {
-            MPI_Status status;
-            MPI_Recv(&resive, 1, MPI_LONG_LONG, i, 0, MPI_COMM_WORLD, &status);
-            result += resive;
+    }
+
+    std::vector<int> locA(v_size + re);
+    std::vector<int> locB(v_size + re);
+
+    if (rank == 0) {
+        locA = std::vector<int>(a.begin(), a.begin() + v_size);
+        locB = std::vector<int>(b.begin(), b.begin() + v_size);
+    } else if (rank == size - 1) {
+        MPI_Status status;
+        MPI_Recv(&locA[0], v_size + re, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+        MPI_Recv(&locB[0], v_size + re, MPI_INT, 0, 3, MPI_COMM_WORLD, &status);
+    } else {
+        MPI_Status status;
+        MPI_Recv(&locA[0], v_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&locB[0], v_size, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+    }
+
+    if (rank == size - 1) {
+        for (unsigned int i = 0; i < v_size + re; ++i) {
+            result_other += locA[i] * locB[i];
         }
     } else {
-        if (rank == size - 1) {
-            for (unsigned int i = v_size * rank; i < a.size(); i++) {
-                result_other += a[i] * b[i];
-            }
-        } else {
-            for (int i = v_size * rank; i < v_size * (rank + 1); i++) {
-                result_other += a[i] * b[i];
-            }
+        for (unsigned int i = 0; i < v_size; ++i) {
+            result_other += locA[i] * locB[i];
         }
-        MPI_Send(&result_other, 1, MPI_LONG_LONG, 0, 0, MPI_COMM_WORLD);
     }
+
+    MPI_Reduce(&result_other, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     return result;
 }

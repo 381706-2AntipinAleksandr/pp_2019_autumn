2e172040d95747ea1146700c1a3e2d7909cf26cd
Кристина <miss-yurievna@ya.ru>
2019-11-18 21:09:39 +0300

    emt

--- modules/task_2/kolesova_k_rib_vert_sch/rib_vert_sch.h
+++ modules/task_2/kolesova_k_rib_vert_sch/rib_vert_sch.h
@@ -1,16 +1,1 @@
-// Copyright 2019 Kolesova Kristina
 
-#ifndef MODULES_TASK_2_KOLESOVA_K_RIB_VERT_SCH_RIB_VERT_SCH_H_
-#define MODULES_TASK_2_KOLESOVA_K_RIB_VERT_SCH_RIB_VERT_SCH_H_
-
-#include <vector>
-
-std::vector<int> randVec(int n);
-
-std::vector<int> randMatr(int m, int n);
-
-std::vector<int> calcMatrOneProc(std::vector<int> matr, std::vector<int> vec, int n, int m);
-
-std::vector<int> calcMatr(std::vector<int> matr, std::vector<int> vec, int n, int m);
-
-#endif  // MODULES_TASK_2_KOLESOVA_K_RIB_VERT_SCH_RIB_VERT_SCH_H_

23fc4d14a27d4895036a19c2e50924cc6f461fc5
Кристина <miss-yurievna@ya.ru>
2020-02-21 23:03:47 +0300

    test3

--- modules/task_3/kolesova_k_sort_betch/sort_betch.h
+++ modules/task_3/kolesova_k_sort_betch/sort_betch.h
@@ -1,6 +1,6 @@
 // Copyright 2019 Kolesova Kristina
 #ifndef MODULES_TASK_3_KOLESOVA_K_SORT_BETCH_SORT_BETCH_H_
-#define MODULES_TASK_3_KOLESOVA_K_SORT_BETCH_SORT_BETCH_H_ 
+#define MODULES_TASK_3_KOLESOVA_K_SORT_BETCH_SORT_BETCH_H_
 
 #include <mpi.h>
 #include <vector>
@@ -16,4 +16,4 @@ std::vector<double> last_razr(std::vector<double> vec1, std::vector<double> vec2
 void sort(std::vector<int> vec, int size);
 std::vector<int> merge_batcher(std::vector<int> global_vec, int size_vec);
 
-#endif // MODULES_TASK_3_KOLESOVA_K_SORT_BETCH_SORT_BETCH_H_
+#endif  // MODULES_TASK_3_KOLESOVA_K_SORT_BETCH_SORT_BETCH_H_
